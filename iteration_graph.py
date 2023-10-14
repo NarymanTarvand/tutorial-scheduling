3bbedd065554758053368555bc5fb720dbf6ed37
izzhann <132635279+izzhann@users.noreply.github.com>
2023-10-14 18:46:55 +1100

    Update iteration_graph.py
    Updated to add for instance 2

--- iteration_graph.py
+++ iteration_graph.py
@@ -1,14 +1,16 @@
 from tabu_search_newest import *
 
 # start each iteration at 1
-ga_iterations = list(range(1, 101))
-ga_best_sols = [43, 33, 32, 31, 31, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30,
+
+# INSTANCE 1
+ga_iterations_i1 = list(range(1, 101))
+ga_best_sols_i1 = [43, 33, 32, 31, 31, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30,
                         30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30,
                         30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30,
                         30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30]
 
-tabu_iterations = list(range(1,159))
-tabu_best_sols = [47, 46, 44, 43, 42, 41, 40, 40, 40, 39, 39, 39, 39, 38, 38, 37, 37, 37, 37, 36, 36, 36, 36, 36, 36,
+tabu_iterations_i1 = list(range(1,159))
+tabu_best_sols_i1 = [47, 46, 44, 43, 42, 41, 40, 40, 40, 39, 39, 39, 39, 38, 38, 37, 37, 37, 37, 36, 36, 36, 36, 36, 36,
                   36, 36, 36, 36, 36, 36, 36, 36, 36, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35,
                   35, 35, 35, 35, 35, 35, 35, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34,
                   34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34,
@@ -16,19 +18,42 @@ tabu_best_sols = [47, 46, 44, 43, 42, 41, 40, 40, 40, 39, 39, 39, 39, 38, 38, 37
                   34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34,
                   34, 34, 34, 34, 34, 34, 34, 34]
 
-# start each iteration at 1
-
 # add in GRASP here
-grasp_iterations = list(range(1, 101))
-grasp_best_sols = []
-all_iters = [ga_iterations, tabu_iterations, grasp_iterations]
-all_best_sols = [ga_best_sols, tabu_best_sols, grasp_best_sols]
+grasp_iterations_i1 = list(range(1, 101))
+grasp_best_sols_i1 = []
+
+all_iters_i1  = [ga_iterations_i1, tabu_iterations_i1, grasp_iterations_i1]
+all_best_sols_i1 = [ga_best_sols_i1, tabu_best_sols_i1, grasp_best_sols_i1]
+
+# INSTANCE 2
+
+# add in ga instance 2
+ga_iterations_i2 = list(range(1, 101))
+ga_best_sols_i2 = []
+
+
+tabu_iterations_i2 = list(range(1, 259))
+tabu_best_sols_i2 = [78, 77, 76, 76, 75, 75, 74, 74, 74, 74, 73, 72, 72, 72, 72, 72, 72, 72, 72, 71, 70, 69, 69, 69,
+                     68, 67, 67, 66, 65, 65, 65, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+                     64, 64, 64, 64, 64, 63, 63, 63, 63, 63, 63, 63, 63, 62, 62, 62, 62, 62, 62, 62, 62, 62, 62, 62,
+                     62, 62, 62, 61, 61, 61, 61, 61, 61, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60,
+                     60, 60, 59, 59, 59, 59, 59, 59, 59, 59, 59, 59, 59, 59, 59, 59, 59, 59, 58, 58, 58, 58, 58, 58,
+                     58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58,
+                     58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 58, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57,
+                     57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57,
+                     57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57,
+                     57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57,
+                     57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57, 57]
+
+# add in grasp instance 2
+grasp_iterations_i2 = list(range(1, 101))
+grasp_best_sols_i2 = []
 
 def ensure_max_length(lst, max_length):
     if len(lst) < max_length:
         lst.extend([lst[-1]] * (max_length - len(lst)))
 
-def create_graph(iterations, best_sols):
+def create_graph(iterations, best_sols, instance):
     # Intakes a list of iterations lists and a list of best sol lists
     # the lists must be in order of genetic alg, tabu search, GRASP
     max_length = max(len(iteration_list) for iteration_list in iterations)
@@ -49,7 +74,11 @@ def create_graph(iterations, best_sols):
     yticks = np.arange(min_value, max_value + 2, step=2)
     plt.yticks(yticks)
     plt.legend()
-    plt.savefig('iterations_best_sol.png')
+    if instance == 1:
+        plt.savefig('iterations_best_sol_i1.png')
+    if instance == 2:
+        plt.savefig('iterations_best_sol_i2.png')
     plt.show()
 
-create_graph(all_iters, all_best_sols)
+create_graph(all_iters_i1, all_best_sols_i1, 1)
+create_graph(all_iters_i1, all_best_sols_i1, 2)
